622e2c40591506804c773b846f5c3d7df1038440
Felix Giese <felix.giese@icloud.com>
2024-04-28 07:43:25 +0200

    fix invalid entry in rootset

--- gc/src/pointer/rootset/pointerslab.rs
+++ gc/src/pointer/rootset/pointerslab.rs
@@ -1,6 +1,6 @@
 pub struct Pointerslab {
     storage: Vec<u32>,
-    free_top: Option<Slabkey>,
+    free_slots: Vec<Slabkey>,
 }
 
 pub struct Slabkey(pub u32);
@@ -9,7 +9,7 @@ impl Pointerslab {
     pub const fn new() -> Self {
         Self {
             storage: Vec::new(),
-            free_top: None,
+            free_slots: Vec::new(),
         }
     }
 
@@ -22,14 +22,11 @@ impl Pointerslab {
     }
 
     pub fn insert(&mut self, value: u32) -> Slabkey {
-        if let Some(free_spot) = self.free_top.take() {
-            let slot = self.get_mut(&free_spot);
-            let next_free = core::mem::replace(slot, value);
-            if next_free != u32::MAX {
-                self.free_top = Some(Slabkey(next_free));
-            }
+        if let Some(free_slot) = self.free_slots.pop() {
+            let slot = self.get_mut(&free_slot);
+            *slot = value;
             // the previously filled free spot is now our key
-            free_spot
+            free_slot
         } else {
             let spot = self.storage.len() as u32;
             self.storage.push(value);
@@ -39,10 +36,9 @@ impl Pointerslab {
 
     /// remove the key, return the previously stored value
     pub fn remove(&mut self, key: Slabkey) -> u32 {
-        let next_free = self.free_top.take().map_or(u32::MAX, |k| k.0);
-        let res = core::mem::replace(self.get_mut(&key), next_free);
-        self.free_top = Some(key);
-        res
+        let result = core::mem::replace(self.get_mut(&key), u32::MAX);
+        self.free_slots.push(key);
+        result
     }
 
     pub fn iter(&self) -> impl Iterator<Item = u32> + '_ {
